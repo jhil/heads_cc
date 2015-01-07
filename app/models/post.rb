6bf224030863a3f0374d19c959505384bc5ddaaf
Jeff Hilnbrand <jahilnbrand@gmail.com>
2015-01-07 16:52:09 -0500

    remove related posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { large: "100x100>", medium: "60x60>", small: "30x30>" }
+	has_attached_file :image, styles: { large: "100x100>", medium: "72x72>", small: "32x32>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
