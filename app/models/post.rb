17f54ca86da830e15198a9ba1b0902df3a975700
Jeff Hilnbrand <jahilnbrand@gmail.com>
2015-01-07 11:01:40 -0500

    more interface changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+	has_attached_file :image, styles: { medium: "60x60>", small: "30x30>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
