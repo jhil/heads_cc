c4487c3b5ba9d774c2c270199a36165520b5a8ce
Jeff Hilnbrand <jahilnbrand@gmail.com>
2015-08-09 16:48:56 -0700

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,26 +10,28 @@ class Post < ActiveRecord::Base
 		slug = self.get_slug
 
 		compressed_filestream = Zip::OutputStream.write_buffer do |zos|
+			puts `ls #{uploads_path}`
 			Dir["#{uploads_path}/**/**"].reject{|f|f==archive}.each do |file|
 				zipfile.add(file.sub(path+'/',''),file)
 			end
-				
-			end
-		end # Outputs zipfile as StringIO
 
-		s3 = Aws::S3.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-		bucket = Aws::S3::Bucket.create(s3, ENV['S3_BUCKET_NAME'])
+		end
+			
+	end # Outputs zipfile as StringIO
 
-		compressed_filestream.rewind
-		bucket.put("/app/public/uploads/#{slug}.zip", compressed_filestream.read, {}, 'authenticated-read')
+	s3 = Aws::S3.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+	bucket = Aws::S3::Bucket.create(s3, ENV['S3_BUCKET_NAME'])
 
-	end
+	compressed_filestream.rewind
+	bucket.put("/app/public/uploads/#{slug}.zip", compressed_filestream.read, {}, 'authenticated-read')
 
-	def get_file_path
-		return $uploads_path+"/#{self.get_slug}"
-	end
+end
 
-	def get_slug
-		return self.title.parameterize
-	end
+def get_file_path
+	return $uploads_path+"/#{self.get_slug}"
+end
+
+def get_slug
+	return self.title.parameterize
+end
 end
