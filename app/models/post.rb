2f8eca25a7feca8f7fb0011c22cadfc677791c7c
Jeff Hilnbrand <jahilnbrand@gmail.com>
2015-08-09 17:01:01 -0700

    move aws within end

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,22 +13,20 @@ class Post < ActiveRecord::Base
 		ls= exec("ls #{uploads_path}")
 		puts "helo: #{uploads_path}"
 
-
 		compressed_filestream = Zip::OutputStream.write_buffer do |zos|
 			Dir["#{uploads_path}/**/**"].reject{|f|f==archive}.each do |file|
 				zipfile.add(file.sub(path+'/',''),file)
 			end
 
 		end
-			
+		s3 = Aws::S3.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+		bucket = Aws::S3::Bucket.create(s3, ENV['S3_BUCKET_NAME'])
+
+		compressed_filestream.rewind
+		bucket.put("/app/public/uploads/#{slug}.zip", compressed_filestream.read, {}, 'authenticated-read')
 	end # Outputs zipfile as StringIO
 
-	s3 = Aws::S3.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-	bucket = Aws::S3::Bucket.create(s3, ENV['S3_BUCKET_NAME'])
-
-	compressed_filestream.rewind
-	bucket.put("/app/public/uploads/#{slug}.zip", compressed_filestream.read, {}, 'authenticated-read')
-
+	
 end
 
 def get_file_path
